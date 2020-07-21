52f93957907c129a3676397e199fe58dfb39bcbe
JohnCoene <jcoenep@gmail.com>
2020-07-21 22:55:11 +0200

    add verbose to build + checks

--- R/assertions.R
+++ R/assertions.R
@@ -7,9 +7,10 @@ stopif_no_npm <- function(){
 # stop if npm is not found
 stopif_no_name <- function(name){
   if(missing(name))
-    stop("Stop missing `name`", call. = FALSE)
+    stop("Missing `name`of widget", call. = FALSE)
 }
 
+# check if it is a package
 stopif_no_package <- function(){
   desc <- fs::file_exists("DESCRIPTION")
   ns <- fs::file_exists("NAMESPACE")
@@ -18,4 +19,15 @@ stopif_no_package <- function(){
 
   if(!is_pkg)
     stop("You must first create a package to house the widget, see `usethis::create_package`.")
+}
+
+# check if SRC present
+stopif_no_npm_init <- function(){
+  package_json <- fs::file_exists("package.json")
+  src_dir <- fs::dir_exists(SRC)
+
+  is_pkg <- all(package_json, src_dir)
+  
+  if(!is_pkg)
+    stop("No widget scaffold, see `scaffold_widget`", call. = FALSE)
 }
--- R/build.R
+++ R/build.R
@@ -2,6 +2,7 @@
 #' 
 #' Build and develop the widget.
 #' 
+#' @inheritParams scaffold_widget
 #' @param mode The configuration mode tells webpack to use 
 #' its built-in optimizations accordingly.
 #' 
@@ -14,14 +15,17 @@
 #' 
 #' @name build
 #' @export 
-build_widget <- function(mode = c("production", "development", "none")){
+build_widget <- function(mode = c("production", "development", "none"), verbose = interactive()){
+  stopif_no_npm_init()
+  
   mode <- match.arg(mode)
   args <- sprintf("run %s", mode)
-  npm_run(args)
+  npm_run(args, verbose)
 }
 
 #' @rdname build
 #' @export 
 watch_widget <- function(){
+  stopif_no_npm_init()
   npm_run("run watch")
 }
