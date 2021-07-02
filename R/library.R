43edd0a3188a066c9574a0130db7ddd97735e927
JohnCoene <jcoenep@gmail.com>
2021-07-02 17:09:57 +0200

    add library

---
+++ R/library.R
@@ -0,0 +1,27 @@
+#' Make Library
+#' 
+#' Adds library settings to webpack config.
+#' This allow exporting JavaScript objects.
+#' 
+#' @param name Name of the library, see details.
+#' @param type Type of the library.
+#' 
+#' @details The functions will be exported
+#' at the specified `name`, e.g.: if
+#' the name is `myLib` then functions can be 
+#' called with `myLib.function();`. 
+#' 
+#' @export 
+make_library <- function(name, type = "umd"){
+	if(missing(name))
+		stop("Missing name", call. = FALSE)
+
+	common <- readLines("./inst/common/webpack.common.js")
+	common[grepl("\\[name\\]\\.js", common)] <- sprintf(
+		"    filename: '[name].js',\n    library: {name: '%s', type: '%s'},", 
+		name, type
+	)
+
+	writeLines(common, "webpack.common.js")
+	invisible()
+}
