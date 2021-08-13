e912a9e600c96cc0ece5886b1f0887a2b08f3e08
JohnCoene <jcoenep@gmail.com>
2021-08-13 17:48:32 +0200

    corrected leprechaun test

--- tests/testthat/test-leprechaun.R
+++ tests/testthat/test-leprechaun.R
@@ -7,11 +7,13 @@ test_that("Input", {
   # keep working directory
   wd <- getwd()
 
-  expect_error(scaffold_leprechaun())
+  expect_error(scaffold_leprechaun(edit = FALSE))
 
   # test bare
   pkg <- create_tmp_package()
   setwd(pkg)
+  expect_error(scaffold_leprechaun(edit = FALSE))
+	file.create(".leprechaun")
   expect_output(scaffold_leprechaun(edit = FALSE))
   expect_error(scaffold_leprechaun(edit = FALSE))
   setwd(wd)
