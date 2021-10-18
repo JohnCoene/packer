42573fab151fbaf2958908dbdbdc78f4209e4a6d
DivadNojnarg <dgranjon@ymail.com>
2021-10-18 23:54:01 +0200

    simplify framework7 scaffold (remove widget component, server.R)

--- inst/templates/framework7/framework7.js
+++ inst/templates/framework7/framework7.js
@@ -3,10 +3,6 @@ import Framework7 from 'framework7';
 // Import Framework7 Styles
 import 'framework7/framework7-bundle.min.css';
 
-// Install F7 Components using .use() method on class:
-import Range from 'framework7/esm/components/range/range.js';
-Framework7.use([Range]);
-
 import App from './components/app.f7.jsx';
 let app = new Framework7({
   el: '#app',
