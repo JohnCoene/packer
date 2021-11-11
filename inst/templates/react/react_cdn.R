8749362fb163101721e3fa6ffd7140ed0327b762
JohnCoene <jcoenep@gmail.com>
2021-11-11 07:29:44 +0100

    fix(react): CDN template updated URL #16

--- inst/templates/react/react_cdn.R
+++ inst/templates/react/react_cdn.R
@@ -5,15 +5,13 @@
 #' @param version Version of React to use, if `NULL` uses the latest
 #' 
 #' @keywords internal
-reactCDN <- function(version = NULL){
+reactCDN <- function(version = 17){
   unpkg <- "https://unpkg.com"
 
-  version_string <- ""
-  if(!is.null(version))
-    version_string <- sprintf("@%d", version)
+  version_string <- sprintf("@%d", version)
 
   react <- sprintf("%s/react%s/umd/react.production.min.js", unpkg, version_string)
-  react_dom <- sprintf("%s/react%s/umd/react-dom.production.min.js", unpkg, version_string)
+  react_dom <- sprintf("%s/react-dom%s/umd/react-dom.production.min.js", unpkg, version_string)
   shiny::singleton(
     shiny::tags$head(
       shiny::tags$script(src = react, crossorigin = NA),
